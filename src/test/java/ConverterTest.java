108a36f503bc0257b6728ed00e18bf9d478a3d9f
Timur <timur.salakhetdinov@gmail.com>
2017-09-11 02:36:46 +0300

    add test for one symbol conversions

--- src/test/java/ConverterTest.java
+++ src/test/java/ConverterTest.java
@@ -8,6 +8,11 @@ public class ConverterTest {
     static {
         dictionary[1] = "I";
         dictionary[5] = "V";
+        dictionary[10] = "X";
+        dictionary[50] = "L";
+        dictionary[100] = "C";
+        dictionary[500] = "D";
+        dictionary[1000] = "M";
     }
 
     private void assertConverts(Integer n, String expected) {
@@ -20,13 +25,14 @@ public class ConverterTest {
     }
 
     @Test
-    public void oneConvertedToI() throws Exception {
+    public void oneSymbolConvertions() throws Exception {
         assertConverts(1, "I");
-    }
-
-    @Test
-    public void fiveConvertedToV() throws Exception {
         assertConverts(5, "V");
+        assertConverts(10, "X");
+        assertConverts(50, "L");
+        assertConverts(100, "C");
+        assertConverts(500, "D");
+        assertConverts(1000, "M");
     }
 
     private String convert(Integer n) {
