11a2147e3ca80e2d27b5e1a278f33e63c8b513d3
Timur <timur.salakhetdinov@gmail.com>
2017-09-14 22:01:56 +0300

    remove duplication from dictionary

--- src/main/java/Converter.java
+++ src/main/java/Converter.java
@@ -5,25 +5,33 @@ public class Converter {
 
     static {
         dictionary.put(1000, "M");
-        dictionary.put(900, "CM");
         dictionary.put(500, "D");
-        dictionary.put(400, "CD");
         dictionary.put(100, "C");
-        dictionary.put(90, "XC");
         dictionary.put(50, "L");
-        dictionary.put(40, "XL");
         dictionary.put(10, "X");
-        dictionary.put(9, "IX");
         dictionary.put(5, "V");
-        dictionary.put(4, "IV");
         dictionary.put(1, "I");
     }
 
     public static String convert(int n) {
         String result = "";
 
-        for (Integer key : dictionary.keySet())
-            for (; n >= key; result += dictionary.get(key), n -= key) ;
+        int div = dictionary.keySet().iterator().next()/10;
+        Integer prevKey = null;
+        for (Integer key : dictionary.keySet()) {
+            for (; n >= key; ) {
+                if (prevKey != null && n + div >= prevKey) {
+                    result += dictionary.get(div) + dictionary.get(prevKey);
+                    n -= prevKey - div;
+                } else {
+                    result += dictionary.get(key);
+                    n -= key;
+                }
+            }
+            prevKey = key;
+            if (div == prevKey)
+                div /= 10;
+        }
 
         return result;
     }
