74ad21be65cbeeb039a0b6a7beacc68a00367c8b
Timur <timur.salakhetdinov@gmail.com>
2017-09-14 20:37:25 +0300

    clean code

--- src/main/java/Converter.java
+++ src/main/java/Converter.java
@@ -1,28 +1,23 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Converter {
-    private static Map<Integer, String> dictionary = new HashMap<>();
+    private static Map<Integer, String> dictionary = new LinkedHashMap<>();
 
     static {
-        dictionary.put(1, "I");
-        dictionary.put(4, "IV");
-        dictionary.put(5, "V");
-        dictionary.put(9, "IX");
-        dictionary.put(10, "X");
         dictionary.put(40, "XL");
+        dictionary.put(10, "X");
+        dictionary.put(9, "IX");
+        dictionary.put(5, "V");
+        dictionary.put(4, "IV");
+        dictionary.put(1, "I");
     }
 
     public static String convert(int n) {
-        if (dictionary.containsKey(n))
-            return dictionary.get(n);
-        if (n > 40)
-            return dictionary.get(40) + convert(n-40);
-        if (n > 10)
-            return dictionary.get(10) + convert(n-10);
-        if (n > 5)
-            return dictionary.get(5) + convert(n-5);
+        String result = "";
 
-        return dictionary.get(1) + convert(n-1);
+        for (Integer key : dictionary.keySet())
+            for (; n >= key; result += dictionary.get(key), n -= key) ;
+
+        return result;
     }
 }
